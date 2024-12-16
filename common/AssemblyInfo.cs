d07dceb952bf9dde640ac3e5a1b7bca20669a5d7
ThirtySomething <developer@derpaul.net>
2024-12-16 12:07:26 +0100

    #UPD Unify AssemblyInfo.cs

--- common/AssemblyInfo.cs
+++ common/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("net.derpaul.cdstats.common")]
+[assembly: AssemblyTitle("Common functionality")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net.derpaul.cdstats.common")]
+[assembly: AssemblyProduct("CDStats")]
 [assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- plugins/duration_collection/AssemblyInfo.cs
+++ plugins/duration_collection/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("net.derpaul.cdstats.plugin")]
+[assembly: AssemblyTitle("Plugin: Durations")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net.derpaul.cdstats.plugin")]
+[assembly: AssemblyProduct("CDStats")]
 [assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
